5fc18dfe4d13602c9b0c49c033968f8a450f2594
gitt <tasneemhatem42@gmail.com>
2023-09-19 07:56:53 +0300

    make swar os quran

--- lib/Home/radio/radio_tap.dart
+++ lib/Home/radio/radio_tap.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:islamii2/my_theme.dart';
 
 class RadioTap extends StatelessWidget {
   const RadioTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(color:Colors.orangeAccent);
+    return Container(
+        margin: EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/radio_brown.png',
+            ),
+            SizedBox(height: 40.0),
+            Text(
+              'اذاعه القران الكريم',
+              style: TextStyle(
+                  color: MyTheme.blackcolor,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25),
+            ),
+            SizedBox(height: 40.0),
+            Image.asset('assets/images/Group 5.png')
+          ],
+        ));
   }
 }
